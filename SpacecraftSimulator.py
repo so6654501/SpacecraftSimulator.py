45bb34eda6bb8dcd3d6c92250e7f49ec6fec4cc1
so6654501 <129784401+so6654501@users.noreply.github.com>
2023-04-18 18:03:01 +0300

    Create SpacecraftSimulator.py

---
+++ SpacecraftSimulator.py
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // Здесь код программы для симуляции полетов космических кораблей
+    return 0;
+}
